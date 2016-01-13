28bf92cc69941fb4378bc7466f91331ae61e3f65
Teilnehmer <Teilnehmer@BBQTEIL-5PGOPGC.Bildung.zz>
2016-01-13 09:34:42 +0100

    "Mitarbeiter zum Projekt"  Mokup hinzugefügt

---
+++ view/Projektmitarbeiter.php
@@ -0,0 +1,41 @@
+<?php
+
+include './header.php';
+?>
+
+<script type="text/javascript" src="./js/clickListener.js"></script>
+
+<div id='cssmenu'>
+    <ul>
+        <li class='active'><a href='#'><span>Mitarbeiter dem Projet zuteilen</span></a></li>
+        <table border="1">
+            <thead>
+
+            </thead>
+            <tbody>
+                <tr>
+                    <th>ID</th>
+                    <th><input type="text" name="ID" value="" /></th>
+                </tr>
+                <tr>
+                    <th>von</th>
+                    <th><input type="datetime" name="Vorname" value="" /></th>
+                </tr>
+                <tr>
+                    <th>bis</th>
+                    <th><input type="text" name="Nachname" value="" /></th>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" value="ok" /></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </ul>
+</div>
+
+
+<?php
+
+include './footer.php';
+?>

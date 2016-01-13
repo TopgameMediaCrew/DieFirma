b15b403a3201c91ed2a3631f2923268d1d61561f
Teilnehmer <Teilnehmer@BBQTEIL-N6GA5CQ.Bildung.zz>
2016-01-13 14:55:37 +0100

    Mitarbeiter, Ausleihe, Aenderbar und Zeitmessbar

---
+++ class/Aenderbar.php
@@ -0,0 +1,13 @@
+<?php
+
+interface Aenderbar
+{
+    public static function insert();
+    public static function delete();
+    public static function update();
+    public static function getById();
+    public static function getAll();
+}
+
+?>
+
